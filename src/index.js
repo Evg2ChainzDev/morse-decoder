0d71ee80d5200445df0f82724b096a5541e99adb
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2022-01-28 16:33:16 +0200

    daone without errors

--- src/index.js
+++ src/index.js
@@ -35,10 +35,32 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+	'*':' '
 };
 
 function decode(expr) {
-    // write your solution here
+       let arr = [];
+    for (let i=0; i<(expr.length/10);i++) {
+        arr[i]=expr.substr( i*10,10)
+    }
+    console.log(arr)
+    const decode1 = function (z){
+        let indexOf1 = z.indexOf('1');
+        z = z.slice(indexOf1)
+        z = z.replace(/10/gm, '.')
+        z = z.replace(/11/gm, '-')
+        return z
+    }
+    arr = arr.map(el=>decode1(el))
+    console.log(arr)
+
+    const findInMorse = function (y) {
+        return MORSE_TABLE[y];
+    }
+
+    arr = arr.map(el=>findInMorse(el))
+    let res = arr.join('');
+    return res
 }
 
 module.exports = {
